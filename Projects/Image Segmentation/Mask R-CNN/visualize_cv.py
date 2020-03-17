3ad58037636c0b2cea7b41ad649a178e3acfc46f
unknown <davit_gadyan@edu.aua.am>
2020-03-17 17:22:48 +0400

    mask-rcnn video capture

---
+++ Projects/Image Segmentation/Mask R-CNN/visualize_cv.py
@@ -0,0 +1,176 @@
+
+import cv2
+import numpy as np
+
+
+def random_colors(N):
+    np.random.seed(1)
+    colors = [tuple(255 * np.random.rand(3)) for _ in range(N)]
+    return colors
+
+
+def apply_mask(image, mask, color, alpha=0.5):
+    """apply mask to image"""
+    for n, c in enumerate(color):
+        image[:, :, n] = np.where(
+            mask == 1,
+            image[:, :, n] * (1 - alpha) + alpha * c,
+            image[:, :, n]
+        )
+    return image
+
+
+def display_instances(image, boxes, masks, ids, names, scores):
+    """
+        take the image and results and apply the mask, box, and Label
+    """
+    n_instances = boxes.shape[0]
+    colors = random_colors(n_instances)
+
+    if not n_instances:
+        print('NO INSTANCES TO DISPLAY')
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == ids.shape[0]
+
+    for i, color in enumerate(colors):
+        if not np.any(boxes[i]):
+            continue
+
+        y1, x1, y2, x2 = boxes[i]
+        label = names[ids[i]]
+        score = scores[i] if scores is not None else None
+        caption = '{} {:.2f}'.format(label, score) if score else label
+        mask = masks[:, :, i]
+
+        image = apply_mask(image, mask, color)
+        image = cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+        image = cv2.putText(
+            image, caption, (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.7, color, 2
+        )
+
+    return image
+
+
+if __name__ == '__main__':
+    """
+        test everything
+    """
+    import os
+    import sys
+    import random
+    import math
+    import numpy as np
+    import skimage.io
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    # Root directory of the project
+    ROOT_DIR = os.path.abspath("../")
+
+    # Import Mask RCNN
+    sys.path.append(ROOT_DIR)  # To find local version of the library
+    from mrcnn import utils
+    import mrcnn.model as modellib
+    from mrcnn import visualize
+    # Import COCO config
+    sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
+    # import coco
+
+
+
+    # Directory to save logs and trained model
+    MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+    # Local path to trained weights file
+    COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+    # Download COCO trained weights from Releases if needed
+    if not os.path.exists(COCO_MODEL_PATH):
+        utils.download_trained_weights(COCO_MODEL_PATH)
+
+    # Directory of images to run detection on
+    IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+    from mrcnn import utils
+    import mrcnn.model as modellib
+    from mrcnn.config import Config
+    from mrcnn import visualize
+
+    class CocoConfig(Config):
+        """Configuration for training on MS COCO.
+        Derives from the base Config class and overrides values specific
+        to the COCO dataset.
+        """
+        # Give the configuration a recognizable name
+        NAME = "coco"
+
+        # We use a GPU with 12GB memory, which can fit two images.
+        # Adjust down if you use a smaller GPU.
+        IMAGES_PER_GPU = 2
+
+        # Uncomment to train on 8 GPUs (default is 1)
+        # GPU_COUNT = 8
+
+        # Number of classes (including background)
+        #NUM_CLASSES = 37  # COCO has 80 classes
+        NUM_CLASSES = 81
+
+    #class InferenceConfig(coco.CocoConfig):
+    class InferenceConfig(CocoConfig):
+        # Set batch size to 1 since we'll be running inference on
+        # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
+        #MAX_GT_INSTANCES = 100
+        #TRAIN_ROIS_PER_IMAGE = 50
+        #BACKBONE = "resnet50" #not working at all!
+        #RPN_ANCHOR_STRIDE = 2
+        POST_NMS_ROIS_TRAINING = 1000
+        POST_NMS_ROIS_INFERENCE = 500
+        IMAGE_MIN_DIM = 400 #really much faster but bad results
+        IMAGE_MAX_DIM = 512
+        #DETECTION_MAX_INSTANCES = 50 #a little faster but some instances not recognized
+
+    config = InferenceConfig()
+    config.display()
+    model = modellib.MaskRCNN(
+        mode="inference", model_dir=MODEL_DIR, config=config
+    )
+    model.load_weights(COCO_MODEL_PATH, by_name=True)
+    class_names = [
+        'BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+        'bus', 'train', 'truck', 'boat', 'traffic light',
+        'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+        'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+        'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+        'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+        'kite', 'baseball bat', 'baseball glove', 'skateboard',
+        'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+        'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+        'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+        'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+        'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+        'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+        'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+        'teddy bear', 'hair drier', 'toothbrush'
+    ]
+
+    capture = cv2.VideoCapture(0)
+    out = cv2.VideoWriter('mask-rcnn.mp4', -1, 20.0, (640,480))
+    # these 2 lines can be removed if you dont have a 1080p camera.
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+    while True:
+        ret, frame = capture.read()
+        results = model.detect([frame], verbose=0)
+        r = results[0]
+        frame = display_instances(
+            frame, r['rois'], r['masks'], r['class_ids'], class_names, r['scores']
+        )
+        cv2.imshow('frame', frame)
+        out.write(frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    capture.release()
+    out.release()
+    cv2.destroyAllWindows()
--- Projects/Image Segmentation/README.md
+++ Projects/Image Segmentation/README.md
@@ -8,3 +8,4 @@ This project aims to demonstrate image segmentation using Mask R-CNN model.
 ![masked_image](https://user-images.githubusercontent.com/35612153/76711618-1746ec00-672b-11ea-9130-702f7dee9284.png)
 
 
+![mask-rcnn](https://user-images.githubusercontent.com/35612153/76859983-b7a82800-6873-11ea-9e7b-836e58c57b43.gif)
